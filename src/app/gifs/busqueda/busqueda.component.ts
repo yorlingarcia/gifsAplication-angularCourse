3617baab666489dca693645a66ce2fb6d05a39a0
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-15 16:37:06 -0500

    se agrego el componente de busqueda y se realiza lectura del buscador

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [],
+})
+export class BusquedaComponent {
+  buscar(termino: string): void {
+    console.log(termino);
+  }
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPagesComponent } from './gifs-pages/gifs-pages.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 @NgModule({
-  declarations: [GifsPagesComponent],
+  declarations: [GifsPagesComponent, BusquedaComponent, ResultadosComponent],
   exports: [GifsPagesComponent],
   imports: [CommonModule],
 })
