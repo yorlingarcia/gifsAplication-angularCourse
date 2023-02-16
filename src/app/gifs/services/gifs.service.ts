2b44fa358e8470cd1d174ce5fb5cf503e97043d7
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-15 20:07:06 -0500

    se limito la entrada a no repeticiones y se coloco todo en minuscula

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,8 +11,12 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift(query);
+  buscarGifs(query: string = '') {
+    query = query.trim().toLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
     console.log(this._historial);
   }
 }
