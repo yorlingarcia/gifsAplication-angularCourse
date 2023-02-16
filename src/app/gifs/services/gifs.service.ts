d1bda708d5ab1de1925185dcc8759ddbf550b557
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-16 08:36:32 -0500

    se uso una instancia de HttpParamas para mejorar la peticion http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
@@ -7,6 +7,7 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 })
 export class GifsService {
   private apiKey: string = 'qs8LlkJCirl2ltX0ojt6fMYsVvJTaVii';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   public resultados: Gif[] = [];
   get historial() {
@@ -29,10 +30,14 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
       localStorage.setItem('historial', JSON.stringify(this.historial));
     }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
     this.htttp
-      .get<SearchGifsResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=qs8LlkJCirl2ltX0ojt6fMYsVvJTaVii&q=${query}&limit=10`
-      )
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
       .subscribe((response) => {
         // se ejecuta cuando hay solucion del get
         console.log(response.data);
